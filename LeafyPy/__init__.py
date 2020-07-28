82f0f93679906f01c08e8cb33ff3d10a40c6fc5e
Michael <michael.grigoryan25@gmail.com>
2020-07-28 18:37:25 +0400

    Minor Bug Fix

--- LeafyPy/__init__.py
+++ LeafyPy/__init__.py
@@ -32,7 +32,7 @@ def cam(device):
     while True:
         ret, frame = cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imshow('Object Recognition', frame)
+        cv2.imshow('Camera', frame)
         if cv2.waitKey(20) & 0xFF == ord('q'):
             break
 
--- app.py
+++ app.py
@@ -1,14 +1,3 @@
-import numpy as np
-import cv2
+import LeafyPy as lf
 
-cap = cv2.VideoCapture(0)
-
-while True:
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('Object Recognition', frame)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+lf.cam(0)
